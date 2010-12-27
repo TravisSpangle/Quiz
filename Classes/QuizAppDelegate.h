d436ecae4e74f99523baddd3ae143b4a71eb485b
Travis Spangle <tspangle@Travis-Spangles-MacBook.local>
2010-12-27 09:05:57 -0800

    Declared variables and methods

--- Classes/QuizAppDelegate.h
+++ Classes/QuizAppDelegate.h
@@ -9,10 +9,23 @@
 #import <UIKit/UIKit.h>
 
 @interface QuizAppDelegate : NSObject <UIApplicationDelegate> {
+	int currentQuestionIndex;
+	
+	//The model Objects
+	NSMutableArray *questions;
+	NSMutableArray *answers;
+	
+	//The view objects
+	IBOutlet UILabel *questionField;
+	IBOutlet UILabel *answerField;
+	
     UIWindow *window;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
+
+- (IBAction)showQuestion:(id)sender;
+- (IBAction)showAnswer:(id)sender;
 
 @end
 
