d3364761981f56be7c33a1314024db8d7724fb8c
Travis Spangle <tspangle@Travis-Spangles-MacBook.local>
2010-12-27 08:48:09 -0800

    Initialized project.

---
+++ Classes/QuizAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  QuizAppDelegate.h
+//  Quiz
+//
+//  Created by Travis Spangle on 12/27/10.
+//  Copyright 2010 Peak Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
